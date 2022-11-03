3929036ca8dab9abc522fb6620d38d18a1e55659
Aaditya Muley <aadityamuley8190@gmail.com>
2022-11-03 09:59:32 -0700

    Project initialized

---
+++ hacs/AssignmentTest.java
@@ -0,0 +1,63 @@
+package hacs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Aaditya Muley
+ * ASUrite : amuley2
+ * ASU ID : 1225526185
+ */
+class AssignmentTest {
+
+    @org.junit.jupiter.api.Test
+    void setDueDate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAssSpec() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isOverDue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addSolution() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAddSolution() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void submitSolution() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSolutionList() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSolution() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSugSolution() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSolutionIterator() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDueDateString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void accept() {
+    }
+}
